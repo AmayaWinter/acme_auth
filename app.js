d646699349e3bbdee5c31a1f5d2edc0a35bb5dbc
Amaya Agha <amaya.agha@gmail.com>
2021-09-07 16:02:19 -0400

    completed

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const path = require('path');
 const volleyball = require('volleyball');
 app.use(volleyball);
 
+const requireToken = async (req, res, next) => {
+  try {
+    console.log(req.headers.authorization);
+    const user = await User.byToken(req.headers.authorization)
+    req.user = user;
+    next();
+  }
+  catch(err) {
+    next(err);
+  }
+}
+
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.post('/api/auth', async (req, res, next) => {
@@ -18,28 +31,27 @@ app.post('/api/auth', async (req, res, next) => {
   }
 });
 
-app.get('/api/auth', async (req, res, next) => {
+app.get('/api/auth', requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   } catch (ex) {
     next(ex);
+  }
+});
+
+
+app.get('/api/user/:id/notes', requireToken, async (req, res, next) => {
+  try {
+    // const user = await User.byToken(req.headers.authorization);
+    res.send(await req.user.getNotes());
+  } catch (error) {
+    next(error);
   }
 });
 
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).send({ error: err.message });
-});
-
-app.get('/api/user/:id/notes', async (req, res, next) => {
-  try {
-    const userNotes = await User.findByPk(req.params.id, {
-      include: Note,
-    });
-    res.send(userNotes);
-  } catch (error) {
-    next(error);
-  }
 });
 
 module.exports = app;
